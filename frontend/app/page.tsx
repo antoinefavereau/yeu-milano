0551f0a180bfd8a1c6a36472ee050e36667330c7
Antoine Favereau <antoinefavereau45@gmail.com>
2024-11-08 20:17:35 +0100

    footer

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,20 +1,24 @@
 "use client";
 
 import { useRef } from "react";
-import BookSection from "../components/BookSection";
-import LogosSection from "../components/LogosSection";
-import PostersSection from "../components/PostersSection";
-import TopSection from "../components/TopSection";
+import BookSection from "@/components/BookSection";
+import LogosSection from "@/components/LogosSection";
+import PostersSection from "@/components/PostersSection";
+import TopSection from "@/components/TopSection";
+import UnderTextSection from "@/components/UnderTextSection";
+import Footer from "@/components/Footer";
 
 export default function Home() {
-    const scrollRef = useRef<HTMLDivElement>(null);
+  const scrollRef = useRef<HTMLDivElement>(null);
 
-    return (
-        <main className="flex min-h-screen flex-col">
-            <TopSection scrollRef={scrollRef} />
-            <PostersSection scrollRef={scrollRef} />
-            <LogosSection />
-            <BookSection />
-        </main>
-    );
+  return (
+    <main className="flex min-h-screen flex-col">
+      <TopSection scrollRef={scrollRef} />
+      <PostersSection scrollRef={scrollRef} />
+      <LogosSection />
+      <BookSection />
+      <UnderTextSection />
+      <Footer />
+    </main>
+  );
 }
